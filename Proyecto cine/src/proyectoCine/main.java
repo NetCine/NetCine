4ad12f9f0a9b751a8f2ba52a67724f4419465cd1
cesar <cesar_991@hotmail.es>
2017-05-13 19:20:20 +0200

    Añadir opcion 3 (salir) en menú principal. Corrección de errores a la hora de crear el código de cliente especial. Añadir verificación de edad al registrarse en la app.

--- Proyecto cine/src/proyectoCine/main.java
+++ Proyecto cine/src/proyectoCine/main.java
@@ -22,7 +22,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.*;
-
+import java.time.*;
 /**
  * Programa principal del proyecto
  * @author cesar
@@ -43,241 +43,265 @@ public class main {
 		System.out.println("|----------------------------------|\n\n");
 		
 		do{
-			try{
-				opcion1=0;
-				System.out.println("¿Que desea hacer?");
-				System.out.println("1.- Para registro en NetCine");
-				System.out.println("2.- Para entrar en NetCine\n");
-				System.out.print("--- Opcion: ");
-				
-				opcion1=sc.nextInt();
-			
-			}catch(InputMismatchException e){			
-				opcion1=3; // Le asigno un valor mayor que el 2 y así imprimo el error abajo, mensaje común para todos los fallos
-				sc.nextLine(); // Limpieza de buffer		
-			}
-			
-			if(opcion1!=1 && opcion1!=2)
-				System.out.println("\n--- ERROR. Opcion no valida, debe ser 1 para registo o 2 para entrar\n ");
-			
-		}while(opcion1!=1 && opcion1!=2);
-		
-		/*REGISTRARSE*/
-		if(opcion1==1){ // Vamos al registro en la aplicacion
-			
-			/*Desde la ventana de usuario solo se puede dar de alta un cliente nuevo, recogemos sus datos
-			 el empleado solo lo puede dar de alta el jefe de cine por lo que aqui solo se registran clientes*/
-			
-			int error=0; // variable que se va a encargar de comenzar de nuevo a pedir datos en caso de que sale la excepcion
-			
-			String nombre="",apellidos="",dni="",telefono="",correo="",clave="";
-			int tarjeta=0, dia=0,mes=0,anyo=0, tipoCliente=0;
-			LocalDate fechaNacimiento=null;
-			
-			String [] codigosPromocionales = new String [10]; // Array donde tenemos los codigos promocionales para poder registrarse como usuario especial
-			int contadorCodigos=0;
-			
-			codigosPromocionales[0] = "CUPON1";
-			contadorCodigos++;
-			codigosPromocionales[1] = "PROMOCIONESPECIAL";
-			contadorCodigos++;
-			
 			do{
 				try{
+					opcion1=0;
+					System.out.println("\n¿Que desea hacer?");
+					System.out.println("1.- Para registro en NetCine");
+					System.out.println("2.- Para entrar en NetCine");
+					System.out.println("3.- Para salir\n");
+					System.out.print("--- Opcion: ");
 					
-					do{ // Recogemos el tipo de cliente que va a darse de alta
-						System.out.print("\nIntroduce tipo de cliente \n1.- Normal\n2.- Especial\nOpcion: ");
-						tipoCliente=sc.nextInt();
-						
-						if(tipoCliente!=1 && tipoCliente!=2)
-							System.out.println("\n--- ERROR, tipo de cliente no valido, inserte de nuevo");
-						
-					}while(tipoCliente!=1 && tipoCliente!=2);
-						
-					
-					/*AÑADIR METER CODIGO PROMOCIONAL PARA PODER REGISTRARSE COMO ESPECIAL*/
-					if(tipoCliente==2){
-						int correcto=0;
-						sc.nextLine();
-						do{
-							System.out.println("\nPara poder darte de alta como usuario especal necesitas un codigo promocional.\nIntroduce un codigo promocional valido para poder registrarte como usuario especial:\n ");
-							String leerCodigo=sc.nextLine();
-							
-							for(int i=0;i<contadorCodigos;i++){ // Buscamos si el codigo es correcto
-								if(codigosPromocionales[i].equals(leerCodigo.toUpperCase())){
-									correcto++;								
-								}									
-							}
-							
-							if(correcto==0)
-								System.out.print("\n--- ERROR, codigo no valido, inserta un codigo promocional valido para registrarte como usuario especial ");
-							
-						}while(correcto==0);	
-						
-					}
-					
-					if(tipoCliente==1)// Vendremos de leer un int por lo que estamos obligados a limpiar el buffer
-						sc.nextLine();
-					
-					error=0;
-					
-					System.out.print("\nIntroduce nombre: ");
-					nombre=sc.nextLine();
-					System.out.print("Introduce apellidos: ");
-					apellidos=sc.nextLine();
-					System.out.print("Introduce DNI: ");
-					dni=sc.nextLine(); // comprobar que sea un dni valido.
-					System.out.print("Introduce telefono de contacto: ");
-				    telefono=sc.nextLine();
-				    System.out.print("Introduce dia de nacimiento: ");
-					dia=sc.nextInt();					
-					System.out.print("Introduce mes de nacimiento: ");
-					mes=sc.nextInt();
-					System.out.print("Introduce año de nacimiento: ");
-					anyo=sc.nextInt();					
-					fechaNacimiento=(LocalDate.of(anyo,mes,dia)); // Se genera la fecha
-					System.out.print("Introduce numero de tarjeta de credito: ");
-					tarjeta=sc.nextInt();
-					sc.nextLine(); // Limpiamos el buffer					
-					
-					int correoValido;
-					/* Al añadir el mail, verificamos que sea correcto comprobando que tenga una @*/
-					do{	
-						System.out.print("Introduce correo: ");
-						correo=sc.nextLine();
-						
-						correoValido=1;
-						if(correo.indexOf("@")==-1){							
-							System.out.println("\n--- Correo no válido, inserte de nuevo.");
-							correoValido=0;
-						}
-					}while(correoValido==0);	
-					
-					System.out.print("Introduce contraseña: ");
-					clave=sc.nextLine();
+					opcion1=sc.nextInt();
 				
-				}catch(InputMismatchException e){
-					sc.nextLine(); // Limpieza buffer
-								
-					error++;
-					System.out.println("\n--- ERROR, datos erroneos, empiece de nuevo\n");
-				}	
-								
-			}while(error!=0); // Si error se incrementa repetimos el proceso ya que ha saltado una excepcion
-			
-			/*Vamos a añadir el cliente normal a la tabla cliente*/
-			if(tipoCliente==1){
-				String codigoClienteNormal=crearCodigoClienteNormal(bd);
-				
-				// Le creamos sin descuento por ser cliente normal
-				Cliente cl= (new Cliente(codigoClienteNormal,nombre,apellidos,dni,telefono,correo,clave,tarjeta,0,fechaNacimiento));// Creamos el objeto que vamos a insertar en la bbdd para dar el alta
-				
-				if ( bd.añadirCliente(cl)) // Insertamos en la bbdd
-					System.out.println("Añadido en la tabla clientes");		
-				else
-					System.out.println("--- No se ha podido añadir en la tabla clientes");
-				
-				/*Vamos a añadirlo en la tabla usuarios*/				
-				Usuario usu = (new Usuario(codigoClienteNormal,correo,clave)); // Generamos el usuario para insertarle
-				
-				if(bd2.añadirUsuario(usu))
-					System.out.println("Añadido en la tabla usuarios");
-				else
-					System.out.println("--- No se ha podido añadir a la tabla usuarios");
-			}
-			
-			/*Vamos a añadir el cliente especial a la tabla cliente*/
-			if(tipoCliente==2){
-															
-				String codigoClienteEspecial=crearCodigoClienteEspecial(bd);
-				
-				// Le creamos con el descuento de cliente especial (30%)
-				Cliente cl= (new Cliente(codigoClienteEspecial,nombre,apellidos,dni,telefono,correo,clave,tarjeta,30,fechaNacimiento));// Creamos el objeto que vamos a insertar en la bbdd para dar el alta
-				if ( bd.añadirCliente(cl)) // Insertamos en la bbdd
-					System.out.println("Añadido en la tabla clientes");		
-				else
-					System.out.println("--- No se ha podido añadir en la tabla clientes");
-				
-				/*Vamos a añadirlo en la tabla usuarios*/
-				
-				Usuario usu = (new Usuario(codigoClienteEspecial,correo,clave)); // Generamos el usuario para insertarle
-				
-				if(bd2.añadirUsuario(usu))
-					System.out.println("Añadido en la tabla usuarios");
-				else
-					System.out.println("--- No se ha podido añadir a la tabla usuarios");
-				
-				
-			}
-		}
-		
-		/*CONECTARSE*/
-		if(opcion1==2){ // Vamos a conectarnos y en caso de conectarse, mostramos las opciones correctas para ese usuario
-			
-			sc.nextLine();
-			System.out.print("\nIntroduce correo: ");
-			String correo=sc.nextLine();
-			System.out.print("Introduce contraseña: ");
-			String clave=sc.nextLine();
-						
-			Usuario usuario=new Usuario(correo,clave); // Creamos el usuario para poder buscarle
-				
-			String resultadoBusqueda= bd2.buscarUsuario(usuario); // Buscamos el usuario
-			
-			if (resultadoBusqueda==null)
-				System.out.println("Por motivos técnicos no podemos obtener la información en este momento");
-			else
-				if (resultadoBusqueda.equals(""))
-					System.out.println("LO SENTIMOS PERO ESE USUARIO NO ESTA REGISTRADO");
-			
-				else{
-					
-					String cadena = correo+"\t"+clave+"\t"+LocalDate.now();
-					
-					escribirConexionFichero(cadena);
-					
-					String codigo = resultadoBusqueda.substring(0, resultadoBusqueda.length()-4); // Eliminamos los 4 ultimos caracteres para quedarnos con las dos letras y con ellas saber el tipo de usuario.
-					
-					/*Segun el usuario conectado sacamos un menu u otro*/
-					if(codigo.indexOf("CN")!=-1){
-						
-						System.out.println("SE HA CONECTADO COMO USUARIO CLIENTE NORMAL, BIENVENIDO");
-						
-						/* APARTADO DEL CLIENTE NORMAL*/
-						
-					}
-					
-					if(codigo.indexOf("CE")!=-1){
-					
-						System.out.println("SE HA CONECTADO COMO USUARIO CLIENTE ESPECIAL, BIENVENIDO");
-						/*APARTADO CLIENTE ESPECIAL*/
-						
-					}
-					
-					
-					if(codigo.indexOf("JF")!=-1){
-						
-						// Hacer funcion para que jefe del cine pueda añadir nuevo cupon para clientes especial
-						
-						/*METODO PARA EL JEFE DEL CINE*/
-						System.out.println("\nSE HA CONECTADO COMO JEFE CINE, BIENVENIDO");
-						
-					}
-					
-					
-					if(codigo.indexOf("EM")!=-1){
-						
-						/*METODO PARA EL EMPLEADO DEL CINE*/
-						
-						System.out.println("SE HA CONECTADO COMO USUARIO EMPLEADO NORMAL, BIENVENIDO");
-					}
-				
-						
+				}catch(InputMismatchException e){			
+					opcion1=3; // Le asigno un valor mayor que el 2 y así imprimo el error abajo, mensaje común para todos los fallos
+					sc.nextLine(); // Limpieza de buffer		
 				}
+				
+				if(opcion1!=1 && opcion1!=2 && opcion1!=3)
+					System.out.println("\n--- ERROR. Opcion no valida, debe ser 1 para registo o 2 para entrar\n ");
+				
+			}while(opcion1!=1 && opcion1!=2 && opcion1!=3);
+			
+			/*REGISTRARSE*/
+			if(opcion1==1){ // Vamos al registro en la aplicacion
+				
+				/*Desde la ventana de usuario solo se puede dar de alta un cliente nuevo, recogemos sus datos
+				 el empleado solo lo puede dar de alta el jefe de cine por lo que aqui solo se registran clientes*/
+				
+				int error=0; // variable que se va a encargar de comenzar de nuevo a pedir datos en caso de que sale la excepcion
+				
+				String nombre="",apellidos="",dni="",telefono="",correo="",clave="";
+				int tarjeta=0, dia=0,mes=0,anyo=0, tipoCliente=0;
+				LocalDate fechaNacimiento=null;
+				
+				String [] codigosPromocionales = new String [10]; // Array donde tenemos los codigos promocionales para poder registrarse como usuario especial
+				int contadorCodigos=0;
+				
+				codigosPromocionales[0] = "CUPON1";
+				contadorCodigos++;
+				codigosPromocionales[1] = "PROMOCIONESPECIAL";
+				contadorCodigos++;
+				
+				do{
+					try{
+						
+						do{ // Recogemos el tipo de cliente que va a darse de alta
+							System.out.print("\nIntroduce tipo de cliente \n1.- Normal\n2.- Especial\nOpcion: ");
+							tipoCliente=sc.nextInt();
+							
+							if(tipoCliente!=1 && tipoCliente!=2)
+								System.out.println("\n--- ERROR, tipo de cliente no valido, inserte de nuevo");
+							
+						}while(tipoCliente!=1 && tipoCliente!=2);
+							
+						
+						/*AÑADIR METER CODIGO PROMOCIONAL PARA PODER REGISTRARSE COMO ESPECIAL*/
+						if(tipoCliente==2){
+							int correcto=0;
+							sc.nextLine();
+							do{
+								System.out.println("\nPara poder darte de alta como usuario especal necesitas un codigo promocional.\nIntroduce un codigo promocional valido para poder registrarte como usuario especial:\n ");
+								String leerCodigo=sc.nextLine();
+								
+								for(int i=0;i<contadorCodigos;i++){ // Buscamos si el codigo es correcto
+									if(codigosPromocionales[i].equals(leerCodigo.toUpperCase())){
+										correcto++;								
+									}									
+								}
+								
+								if(correcto==0)
+									System.out.print("\n--- ERROR, codigo no valido, inserta un codigo promocional valido para registrarte como usuario especial ");
+								
+							}while(correcto==0);	
+							
+						}
+						
+						if(tipoCliente==1)// Vendremos de leer un int por lo que estamos obligados a limpiar el buffer
+							sc.nextLine();
+						
+						error=0;
+						
+						System.out.print("\nIntroduce nombre: ");
+						nombre=sc.nextLine();
+						System.out.print("Introduce apellidos: ");
+						apellidos=sc.nextLine();
+						System.out.print("Introduce DNI: ");
+						dni=sc.nextLine(); // comprobar que sea un dni valido.
+						System.out.print("Introduce telefono de contacto: ");
+					    telefono=sc.nextLine();
+					    System.out.print("Introduce dia de nacimiento: ");
+						dia=sc.nextInt();					
+						System.out.print("Introduce mes de nacimiento: ");
+						mes=sc.nextInt();
+						System.out.print("Introduce año de nacimiento: ");
+						anyo=sc.nextInt();					
+						
+						fechaNacimiento=(LocalDate.of(anyo,mes,dia)); // Se genera la fecha
+												
+						Period tiempo = Period.between(fechaNacimiento,LocalDate.now()); // Comprobamos la difrencia de fechas para ver si es o no mayor de edad
+						
+						System.out.print(tiempo.getYears());
+						
+						if(tiempo.getYears()<18){ // En caso de ser menor de edad no dejamos que se registre.
+							System.out.println("\n--- Lo sentimos no eres aún mayor de 18 años y por lo tanto no puedes registrarte en NetCine\n");
+							tipoCliente=0;
+							break;
+						}
+						
+						System.out.print("Introduce numero de tarjeta de credito: ");
+						tarjeta=sc.nextInt();
+						sc.nextLine(); // Limpiamos el buffer					
+						
+						int correoValido;
+						/* Al añadir el mail, verificamos que sea correcto comprobando que tenga una @*/
+						do{	
+							System.out.print("Introduce correo: ");
+							correo=sc.nextLine();
+							
+							correoValido=1;
+							if(correo.indexOf("@")==-1){							
+								System.out.println("\n--- Correo no válido, inserte de nuevo.");
+								correoValido=0;
+							}
+						}while(correoValido==0);	
+						
+						System.out.print("Introduce contraseña: ");
+						clave=sc.nextLine();
+						
+						
+						}catch(InputMismatchException e){
+							sc.nextLine(); // Limpieza buffer
+										
+							error++;
+							System.out.println("\n--- ERROR, datos erroneos, empiece de nuevo\n");
+						}	
+										
+					}while(error!=0); // Si error se incrementa repetimos el proceso ya que ha saltado una excepcion
+					
+					/*Vamos a añadir el cliente normal a la tabla cliente*/
+					if(tipoCliente==1){
+						String codigoClienteNormal=crearCodigoClienteNormal(bd);
+						
+						// Le creamos sin descuento por ser cliente normal
+						Cliente cl= (new Cliente(codigoClienteNormal,nombre,apellidos,dni,telefono,correo,clave,tarjeta,0,fechaNacimiento));// Creamos el objeto que vamos a insertar en la bbdd para dar el alta
+						
+						if ( bd.añadirCliente(cl)) // Insertamos en la bbdd
+							System.out.println("\nRegistro completado.\nAñadido en la tabla clientes");		
+						else
+							System.out.println("\n--- No se ha podido añadir en la tabla clientes");
+						
+						/*Vamos a añadirlo en la tabla usuarios*/				
+						Usuario usu = (new Usuario(codigoClienteNormal,correo,clave)); // Generamos el usuario para insertarle
+						
+						if(bd2.añadirUsuario(usu))
+							System.out.println("Añadido en la tabla usuarios");
+						else
+							System.out.println("--- No se ha podido añadir a la tabla usuarios");
+					}
+					
+					/*Vamos a añadir el cliente especial a la tabla cliente*/
+					if(tipoCliente==2){
+																	
+						String codigoClienteEspecial=crearCodigoClienteEspecial(bd);
+						
+						// Le creamos con el descuento de cliente especial (30%)
+						Cliente cl= (new Cliente(codigoClienteEspecial,nombre,apellidos,dni,telefono,correo,clave,tarjeta,30,fechaNacimiento));// Creamos el objeto que vamos a insertar en la bbdd para dar el alta
+						if ( bd.añadirCliente(cl)) // Insertamos en la bbdd
+							System.out.println("\nRegistro completado.\nAñadido en la tabla clientes");		
+						else
+							System.out.println("--- No se ha podido añadir en la tabla clientes");
+						
+						/*Vamos a añadirlo en la tabla usuarios*/
+						
+						Usuario usu = (new Usuario(codigoClienteEspecial,correo,clave)); // Generamos el usuario para insertarle
+						
+						if(bd2.añadirUsuario(usu))
+							System.out.println("Añadido en la tabla usuarios");
+						else
+							System.out.println("--- No se ha podido añadir a la tabla usuarios");
+					
+					
+				}
+				
 			}
-	
+			
+			
+			/*CONECTARSE*/
+			if(opcion1==2){ // Vamos a conectarnos y en caso de conectarse, mostramos las opciones correctas para ese usuario
+				
+				sc.nextLine();
+				System.out.print("\nIntroduce correo: ");
+				String correo=sc.nextLine();
+				System.out.print("Introduce contraseña: ");
+				String clave=sc.nextLine();
+							
+				Usuario usuario=new Usuario(correo,clave); // Creamos el usuario para poder buscarle
+					
+				String resultadoBusqueda= bd2.buscarUsuario(usuario); // Buscamos el usuario
+				
+				if (resultadoBusqueda==null)
+					System.out.println("Por motivos técnicos no podemos obtener la información en este momento");
+				else
+					if (resultadoBusqueda.equals(""))
+						System.out.println("LO SENTIMOS PERO ESE USUARIO NO ESTA REGISTRADO");
+				
+					else{
+						
+						String cadena = correo+"\t"+clave+"\t"+LocalDate.now();
+						
+						escribirConexionFichero(cadena);
+						
+						String codigo = resultadoBusqueda.substring(0, resultadoBusqueda.length()-4); // Eliminamos los 4 ultimos caracteres para quedarnos con las dos letras y con ellas saber el tipo de usuario.
+						
+						/*Segun el usuario conectado sacamos un menu u otro*/
+						if(codigo.indexOf("CN")!=-1){
+							
+							System.out.println("SE HA CONECTADO COMO USUARIO CLIENTE NORMAL, BIENVENIDO");
+							
+							/* APARTADO DEL CLIENTE NORMAL*/
+							
+						}
+						
+						if(codigo.indexOf("CE")!=-1){
+						
+							System.out.println("SE HA CONECTADO COMO USUARIO CLIENTE ESPECIAL, BIENVENIDO");
+							/*APARTADO CLIENTE ESPECIAL*/
+							
+						}
+						
+						
+						if(codigo.indexOf("JF")!=-1){
+							
+							// Hacer funcion para que jefe del cine pueda añadir nuevo cupon para clientes especial
+							
+							/*METODO PARA EL JEFE DEL CINE*/
+							System.out.println("\nSE HA CONECTADO COMO JEFE CINE, BIENVENIDO");
+							
+						}
+						
+						
+						if(codigo.indexOf("EM")!=-1){
+							
+							/*METODO PARA EL EMPLEADO DEL CINE*/
+							
+							System.out.println("SE HA CONECTADO COMO USUARIO EMPLEADO NORMAL, BIENVENIDO");
+						}
+					
+							
+					}
+			
+				}
+			
+			if(opcion1==3)
+				System.out.println("\n--- HASTA PRONTO ---");
+		
+			
+		}while(opcion1!=3);
 		sc.close();
 	}
+	
 	
 	
 	/**
@@ -326,13 +350,13 @@ public class main {
 		// El codigo para un cliente especial se genera con las siglas CN mas los ceros que necesite segun los usuarios que ya haya
 		
 		if(numeroCodigo<10)
-			codigo="CE"+000+numeroCodigo;
+			codigo="CE"+"000"+numeroCodigo;
 		
 		if(numeroCodigo>=10 && numeroCodigo<=99)
-			codigo = "CE"+00+numeroCodigo;
+			codigo = "CE"+"00"+numeroCodigo;
 		
 		if(numeroCodigo>99)
-			codigo = "CE"+0+numeroCodigo;
+			codigo = "CE"+"0"+numeroCodigo;
 		
 		if(numeroCodigo>999)
 			codigo = "CE"+numeroCodigo;
@@ -355,16 +379,16 @@ public class main {
 		// El codigo para un cliente especial se genera con las siglas CN mas los ceros que necesite segun los usuarios que ya haya
 		
 		if(numeroCodigo<10)
-			codigo="CE"+000+numeroCodigo;
+			codigo="CN"+"000"+numeroCodigo;
 		
 		if(numeroCodigo>=10 && numeroCodigo<=99)
-			codigo = "CE"+00+numeroCodigo;
+			codigo = "CN"+"00"+numeroCodigo;
 		
 		if(numeroCodigo>99)
-			codigo = "CE"+0+numeroCodigo;
+			codigo = "CN"+"0"+numeroCodigo;
 		
 		if(numeroCodigo>999)
-			codigo = "CE"+numeroCodigo;
+			codigo = "CN"+numeroCodigo;
 		
 		return codigo;
 		
